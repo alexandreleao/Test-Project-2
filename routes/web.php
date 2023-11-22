2acdc9a98419bb226b2fd08b216d48c676d749a7
alexandreleao <erdleosalgueiro@gmail.com>
2023-11-22 18:57:46 -0300

    Removendo dados do cache

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\PostController;
 use App\Mail\OrderShipped;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -87,4 +87,8 @@ Route::get('destroy-session', function(Request $request){
 Route::get('flash-session', function(Request $request){
     $request->session()->flash('status', 'true');
     return redirect('get-session');
+});
+
+Route::get('forget-cache', function(Request $request){
+   Cache::forget('posts');
 });
